9cc8d06228c497d1360e5f9cae84b6cde6da12b6
Crazy Kiwi <Nasita-Haque>
2016-11-01 16:10:30 -0400

    server step 1 created

--- back/server.js
+++ back/server.js
@@ -1,78 +1,45 @@
-// const express = require('express');
-// const app = express();
-// const mongoose = require('mongoose');
-// const postModel = require('./posts/posts-model');
-// const Post = mongoose.model('Post');
-
-// mongoose.connect('mongodb://localhost/blog-app-test');
-
-// const db = mongoose.connection;
-
-// app.get('/', (req, res) => {
-//   res.send('Hey from the / page');
-// });
-
-// app.get('/posts', (req, res) => {
-//   res.send('Hey from the posts page');
-// });
-
-
-// db.on('open', () => {
-//   console.log('db connection opened!');
-//   app.listen(5555, () => {
-//     console.log('Listening on port 5555');
-//     //Creating a test post everytime the server refreshes
-//     Post.create({title: 'test post 1'}, (err, data) => {
-//       if(err) console.log('Error with database!');
-//       else console.log('Post created!');
-//     })
-//     //Find all posts and log to console
-//     Post.find({}, (err, data) => {
-//       console.log('Database data found!', data);
-//     })
-//     //Find all posts that match specific title
-//     Post.find({title: 'find this title'}, (err, data) => {
-//       console.log('Database data found!', data);
-//     })
-//   });
-// })
-
-// db.on('error', () => {
-//   console.log('error in db connection!');
-// })
-
-
-const express = require('express');
 //node require require on the backend
+const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const postModel = require('./posts/post-model')
-const Post = mongoose.model('Post')
+const postModel = require('./posts/post-model');
+const Post = mongoose.model('Post');
 
-mongoose.connect('mongodb://localhost/blog-app-test');
 //the server needs to wait for the data base to be connected
+mongoose.connect('mongodb://localhost/blog-app-test');
 const db = mongoose.connection;
+
+//ROUTES
+app.get('/posts', (req, res)=>{
+	Post.find({}, (err, data) => {
+			res.send(data[data.length-1]);
+		});
+});
+
+//refresh post url, save page, saved to db 
 app.get('/', (req, res)=>{
-	res.send('found page')
-})
-app.get('/post', (req, res)=>{
-	res.send('post page')
-})
+	Post.create({title: 'hi amber!'}, (err, data)=>{
+		if (err) console.log('you have an error');
+		else console.log('post was created!');
+	})
+	res.send('post page');
+});
 
 db.on('open',()=> { 
-	console.log('connection oppen') 
+	console.log('connection open');
   // we're connected!
 	app.listen(5555, () =>{
-		console.log("listening")
-		Post.create({title: 'test post 1'}, (err, data)=>{
-			if (err) console.log('you have an error');
-			else console.log('post was created!')
-		})
-		// Post.find({}, err,data) => {
-		// 	console.log('database foung', data)
-		// }
-	})
+		console.log("listening");
+		// Post.create({title: 'test post 1'}, (err, data)=>{
+		// 	if (err) console.log('you have an error');
+		// 	else console.log('post was created!');
+		// })
+	 	Post.find({}, (err, data) => {
+			console.log('Database data found!', data);
+		});
+	});
 });
+
 db.on('error', ()=>{
-	console.log('err in db collection')
-})
+	console.log('err in db collection');
+});
