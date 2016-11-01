6f77d76fc2462ee13eb2eec82d22949ef299607a
Ambar Gutierrez <ambar.grz@gmaail.com>
2016-11-01 14:31:40 -0400

    first commit

---
+++ back/posts/post-model.js
@@ -0,0 +1,42 @@
+//BLOG POSTS:
+//Title of blog posts
+//Date posted
+//Blog post text
+//Categories/tags
+//Images for post
+//Comments on posts
+//Author of blog posts
+//archive: Boolean
+
+//AUTHORS:
+//name
+//email
+//password
+//bio
+//articles
+
+//FOLLOWERS/READERS:
+//Users
+//name
+//comments
+//email
+//password
+//date joined
+//age
+
+const mongoose = require('mongoose');
+const blogSchema = mongoose.Schema({
+	title: {type: String, require: true},
+	author: String,
+	date: Date,
+	tags: String,
+})
+const Post = mongoose.model('Post', blogSchema)
+
+
+
+
+
+
+
+
